93338602c8077e2559880c2cdc45fa462900816f
Harry-Zhu-45 <869175239@qq.com>
2024-01-07 15:47:27 +0800

    脚本改进

--- collect.sh
+++ collect.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
 # 设置循环次数
-num_runs=3
+num_runs=10
 
 # 输出文件名
 output_file="msd_data.txt"
 
 # 清空或创建输出文件
-> $output_file
+>$output_file
+
+# List of values
+values=(29.6483883 145.849698 316.013000 486.176302 602.377612)
+# values=(8.245876209129506 42.64173029903293 101.31074409281209 179.0544213242767 268.966773509593 363.05922649040696 452.97157867572326 530.7152559071878 589.384269700967 623.7801237908704)
+# values=(1.3218454386828071 7.429242462300721 20.448998156279607 44.35579276774375 85.59512159890338 152.4321293389935 251.20242463993492 378.0986975526874 510.1482256352902 606.495129089744)
 
 # 循环运行命令并收集数据
-for ((i=1; i<=$num_runs; i++)); do
-    echo "Running iteration $i..."
+for value in "${values[@]}"; do
+    for ((i = 1; i <= $num_runs; i++)); do
+        echo "Running iteration $i with value $value..."
 
-    # 运行命令并将输出保存到临时文件
-    result=$(./build/main.out -l 100 -n 100000)
+        # 运行命令并将输出保存到临时文件
+        result=$(./build/main.out -l "$value" -n 100000)
 
-    # 从输出中提取 MSD 数据
-    msd=$(echo "$result" | grep "MSD:" | awk '{print $2}')
+        # 从输出中提取 MSD 数据
+        msd=$(echo "$result" | grep "MSD:" | awk '{print $2}')
 
-    # 将数据写入输出文件
-    echo "$msd" >> $output_file
+        # 将数据写入输出文件
+        echo "$msd" >>$output_file
+    done
+    echo >>$output_file
 done
 
-echo  # 换行，以确保进度条后面有一行空行
+echo # 换行，以确保进度条后面有一行空行
 echo "Data collection complete. MSD data saved to $output_file"
