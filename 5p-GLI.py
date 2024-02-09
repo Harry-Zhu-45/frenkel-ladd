ac141b9d684991282bf95ea90fb244b869e011c9
Harry-Zhu-45 <869175239@qq.com>
2024-02-09 16:21:16 +0800

    修正了 5-point G-L 积分

--- 5p-GLI.py
+++ 5p-GLI.py
@@ -1,9 +1,6 @@
 #!./venv/bin/python
 
 """
-Author: HarryZ
-Created: 2023-11-04
-Updated: 2024-01-07
 Calulate the integral of a function using 5-point Gaussian-Legendre quadrature
 """
 
@@ -28,56 +25,35 @@ weight_5 = [
     0.2369268850561891
 ]
 
-# 第一种取点
-# lambda_min = 0
-# lambda_max = 632.026
-# lambda_list_5 = [(lambda_max - lambda_min) / 2 * i +
-#                   (lambda_max + lambda_min) / 2 for i in abscissa_5]
+origin_lambda_min = 0
+origin_lambda_max = 632.026
+
+lambda_min = np.log(origin_lambda_min + np.e ** 3.5)
+lambda_max = np.log(origin_lambda_max + np.e ** 3.5)
+tmp = [(lambda_max - lambda_min) / 2 * i +
+       (lambda_max + lambda_min) / 2 for i in abscissa_5]
+lambda_list_5 = [np.e ** i - np.e ** 3.5 for i in tmp]
 # print(lambda_list_5)
 
 lambda_list_5 = [
-    29.6483883,
-    145.849698,
-    316.013000,
-    486.176302,
-    602.377612
+    5.00421035,
+    33.0590995,
+    115.297688,
+    299.738483,
+    544.708655
 ]
 
 msd_list_5 = [
-    0.01795073,
-    0.006699942,
-    0.004176842,
-    0.002769681,
-    0.002439662
+    0.014400099999999999,
+    0.01058859052631579,
+    0.0064644305,
+    0.003898474545454546,
+    0.0025043180000000006
 ]
-
-# 第二种取点
-# lambda_min = np.log(lambda_min + np.e ** 3.5)
-# lambda_max = np.log(lambda_max + np.e ** 3.5)
-# tmp = [(lambda_max - lambda_min) / 2 * i +
-#                   (lambda_max + lambda_min) / 2 for i in abscissa_5]
-# lambda_list_5 = [np.e ** i - np.e ** 3.5 for i in tmp]
-# print(lambda_list_5)
-
-# lambda_list_5 = [
-#     5.00421035,
-#     33.0590995,
-#     115.297688,
-#     299.738483,
-#     544.708655
-# ]
-
-# msd_list_5 = [
-#     0.014400099999999999,
-#     0.01058859052631579,
-#     0.0064644305,
-#     0.003898474545454546,
-#     0.0025043180000000006
-# ]
 
 res = 0.0
 for i in range(5):
-    print(msd_list_5[i] * (lambda_list_5[i] + np.e ** 3.5))
     res += weight_5[i] * msd_list_5[i] * (lambda_list_5[i] + np.e ** 3.5)
+res *= (lambda_max - lambda_min) / 2
 
 print(-res)
