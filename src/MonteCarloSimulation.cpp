dea73f7b0a87e99058e20d9aae954fbd5c7380b2
Harry-Zhu-45 <869175239@qq.com>
2024-02-11 15:42:26 +0800

    固定质心算法bug改正

--- src/MonteCarloSimulation.cpp
+++ src/MonteCarloSimulation.cpp
@@ -44,7 +44,7 @@ MonteCarloSimulation::MonteCarloSimulation(int numParticles)
     }
 
     // 固定质心于原点
-    centerOfMass = centerOfMass * (1 / numParticles);
+    centerOfMass = centerOfMass * (1.0 / numParticles);
     for (int i = 0; i < numParticles; ++i)
     {
         lattice[i].position = lattice[i].position - centerOfMass;
@@ -74,9 +74,9 @@ double MonteCarloSimulation::calculateSD()
 void MonteCarloSimulation::metropolisStep()
 {
     // 创建随机数生成器对象
-    std::random_device rd;                                         // 用于种子生成
-    std::mt19937 gen(rd());                                        // Mersenne Twister 伪随机数生成器
-    std::uniform_real_distribution<double> randomDouble(0.0, 1.0); // 生成(0, 1)之间的均匀分布随机数
+    std::random_device rd;                         // 用于种子生成
+    std::mt19937 gen(rd());                        // Mersenne Twister 伪随机数生成器
+    std::uniform_real_distribution<> randomDouble; // 生成属于[0,1)的均匀分布随机数
 
     // 随机选择粒子，直到每个粒子都被选择一次
     std::vector<bool> particleSelected(numParticles, false); // 初始化为 false，表示所有粒子未被选择
@@ -88,48 +88,42 @@ void MonteCarloSimulation::metropolisStep()
         {
             randomIndex = std::uniform_int_distribution<int>(0, numParticles - 1)(gen);
         } while (particleSelected[randomIndex]);
-
         particleSelected[randomIndex] = true; // 将该粒子标记为已选择
 
-        // 调整位移幅度
-        double del = 0.1;
-        // 产生随机位移
-        Vector3D displacement(randomDouble(gen) - 0.5, randomDouble(gen) - 0.5, randomDouble(gen) - 0.5);
-        displacement = displacement * del;
-        // 虚拟位移
-        Vector3D virtualDisplacement = particles[randomIndex].position + displacement;
+        Vector3D displacement(randomDouble(gen) - 0.5, randomDouble(gen) - 0.5, randomDouble(gen) - 0.5); // 产生随机位移
+        double del = 0.14;                                                                                // 随机位移的幅度
+        displacement = displacement * del;                                                                // 调整随机位移的幅度
+        Vector3D virtualDisplacement = particles[randomIndex].position + displacement;                    // 被选中粒子虚拟位移后的位置
 
-        // 先计算位移前后的势能变化，再判断是否接受位移
+        // 先判断 Metroplois 准则，再判断是否接受位移
         // deltaEnergy = lambda (2 delta r_i dot delta_i + (N-1)/N delta_i^2)
-        double deltaEnergy = lambda * 2 * (particles[randomIndex].position - lattice[randomIndex].position).dot(displacement) + lambda * (numParticles - 1) / numParticles * displacement.dot(displacement);
+        double deltaEnergy = lambda * 2 * (particles[randomIndex].position - lattice[randomIndex].position).dot(displacement) + lambda * (1 - 1.0 / numParticles) * displacement.dot(displacement);
 
-        // Metropolis 准则
-        if (randomDouble(gen) < exp(-deltaEnergy / temperature))
+        if (randomDouble(gen) < exp(-deltaEnergy / temperature)) // Metroplois 准则
         {
             // 判断是否存在重叠
-            bool overlap = false; // 是否重叠，初始值为 false
+            bool overlap = false; // 表示是否重叠，初始值为 false
             for (int i = 0; i < numParticles; ++i)
             {
                 if (i != randomIndex) // 遍历除选中粒子外的所有粒子
                 {
-                    Vector3D delta = particles[i].position - virtualDisplacement;
+                    Vector3D delta = particles[i].position - virtualDisplacement; // 粒子间的相对位移
 
-                    // 27 种位移，相当于周期性边界条件
-                    Vector3D displacement[27];
+                    Vector3D displacements[27]; // 相当于周期性边界条件
                     for (int dx = -1; dx <= 1; dx++)
                     {
                         for (int dy = -1; dy <= 1; dy++)
                         {
                             for (int dz = -1; dz <= 1; dz++)
                             {
-                                displacement[(dx + 1) * 9 + (dy + 1) * 3 + (dz + 1)] = delta + Vector3D(dx * nx * latticeConstant / sqrt(2.0), dy * ny * latticeConstant / sqrt(2.0), dz * nz * latticeConstant / 2);
+                                displacements[(dx + 1) * 9 + (dy + 1) * 3 + (dz + 1)] = delta + Vector3D(dx * nx * latticeConstant / sqrt(2.0), dy * ny * latticeConstant / sqrt(2.0), dz * nz * latticeConstant / 2);
                             }
                         }
                     }
 
                     for (int j = 0; j < 27; j++)
                     {
-                        if (displacement[j].magnitude() < 2 * particleRadius)
+                        if (displacements[j].dot(displacements[j]) < sigma * sigma)
                         {
                             overlap = true;
                             break;
@@ -142,7 +136,7 @@ void MonteCarloSimulation::metropolisStep()
                 particles[randomIndex].position = virtualDisplacement;
                 for (int i = 0; i < numParticles; ++i)
                 {
-                    particles[i].position = particles[i].position - displacement * (1 / numParticles);
+                    particles[i].position = particles[i].position - displacement * (1.0 / numParticles);
                 }
                 acceptedMoves++;
             }
